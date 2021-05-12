2d65239b3024599331d3a8f13e8566e240cc72f5
lblum <lblum@student.ethz.ch>
2021-05-12 20:15:30 +0200

    incorporating renaming of IOHandler in __init__.py

--- Project1/Project1/Task1/pipeline/__init__.py
+++ Project1/Project1/Task1/pipeline/__init__.py
@@ -1,5 +1,5 @@
 # __init__.py
 from .datahandler import Datahandler
-from .io_handler import IO_Handler
+from .io_handler import IOHandler
 from .network import Network1, init_xavier, fit, fit_custom
 from .testing import Testing
