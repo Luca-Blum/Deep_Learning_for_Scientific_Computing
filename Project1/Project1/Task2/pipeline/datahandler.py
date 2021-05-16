a4a983d4c9d5fef08cfd1115c94a96ea42e14e74
lblum <lblum@student.ethz.ch>
2021-05-17 00:18:45 +0200

    scaling, differences of targets, create final submission, plot initial data

--- Project1/Project1/Task2/pipeline/datahandler.py
+++ Project1/Project1/Task2/pipeline/datahandler.py
@@ -4,10 +4,11 @@ import pandas as pd
 import torch
 import numpy as np
 from sklearn import preprocessing
-from pipeline import predict, IOHandler
 from pathlib import Path
 from os import path
 import matplotlib.pyplot as plt
+
+from pipeline import IOHandler
 
 
 class Datahandler:
@@ -20,6 +21,7 @@ class Datahandler:
         """
 
         # TODO: scale targets
+        # TODO: plot all
 
         self.training_dfs = {}
         self.names = training_names
@@ -29,14 +31,26 @@ class Datahandler:
         self.predictors = self.header[0:8]
         self.target = ['y']
 
+        original_dfs = {}
         for idx, file in enumerate(training_txt_file):
-            self.training_dfs[self.names[idx]] = pd.read_csv(file, names=self.header, sep=' ')
-            self.scalers[self.names[idx]] = preprocessing.MinMaxScaler()
+            original_dfs[self.names[idx]] = pd.read_csv(file, names=self.header, sep=' ')
+            self.scalers[self.names[idx]] = preprocessing.StandardScaler()
 
-        for idx, key in enumerate(self.training_dfs):
+        for idx, key in enumerate(original_dfs):
             # print(self.training_dfs[key])
-            # self.training_dfs[key][self.predictors] = self.scalers[self.names[idx]].fit_transform(self.training_dfs[key][self.predictors])
+            # original_dfs[key][self.predictors] = self.scalers[self.names[idx]].fit_transform(original_dfs[key][self.predictors])
             pass
+
+        # TODO: create differences for observables
+
+        previous = self.names[0]
+        self.training_dfs[previous] = original_dfs[previous]
+
+        for name in self.names[1:]:
+            ns = original_dfs[name].shape[0]
+            obs_diff = original_dfs[name].iloc[:ns, -1] - original_dfs[previous].iloc[:ns, -1]
+            self.training_dfs[name] = pd.DataFrame(np.concatenate([original_dfs[name].iloc[:ns, :len(self.predictors)], obs_diff.values.reshape(-1, 1)], 1), columns=self.header)
+            previous = name
 
         self.output_path = None
 
@@ -82,33 +96,52 @@ class Datahandler:
         """
         Creates prediction for Testing data with trained model and writes result to text file
         """
-        predictions = []
-        for idx, model in enumerate(models):
-            predictions.append(predict(model, self.testing_dfs[idx]))
 
-        # TODO: combine predicionts
-        combination = predictions[0]
-        for prediction in predictions[1:]:
-            combination += prediction
-        combination /= len(predictions)
+        combination = torch.zeros((self.testing_dfs[0].shape[0],1))
+
+        for idx, model in enumerate(models):
+            combination = combination + model(self.testing_dfs[idx])
 
         submission = pd.DataFrame(combination.detach().numpy())
 
         submission.to_csv(self.output_path, index=False, header=None, float_format='%.18e')
 
+    def plot_data(self):
+        for name in self.names:
+            plt.plot(range(self.training_dfs[name][['y']].shape[0]), self.training_dfs[name][['y']], label="y")
+            plt.legend()
+            plt.xlabel("x")
+            plt.ylabel("y")
+            plt.show()
+
+    def plot_all(self):
+        plt.plot(self.t_training, self.tf0_scaler.inverse_transform(self.tfo_training.detach().numpy()), label="tf0")
+        plt.plot(self.t_training, self.ts0_scaler.inverse_transform(self.tso_training.detach().numpy()), label="ts0")
+        plt.plot(self.t_testing, self.submission['tf0'].values, label="testing tf0")
+        plt.plot(self.t_testing, self.submission['ts0'].values, label="testing ts0")
+        plt.legend()
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.show()
 
 if __name__ == "__main__":
 
-    iohandler_tf0 = IOHandler('tf0')
-    iohandler_ts0 = IOHandler('ts0')
 
     dirname = path.dirname(__file__)
+    training_filename_101 = path.join(dirname, '..', 'data/TrainingData_101.txt')
+    training_filename_401 = path.join(dirname, '..', 'data/TrainingData_401.txt')
+    training_filename_1601 = path.join(dirname, '..', 'data/TrainingData_1601.txt')
+    testing_filename = path.join(dirname, '..', 'data/TestingData.txt')
 
-    testing_filename = path.abspath(path.join(dirname, "..", "data", "TestingData.txt"))
-    training_filename1 = path.abspath(path.join(dirname, "..", "data", "TrainingData_101.txt"))
-    training_filename2 = path.abspath(path.join(dirname, "..", "data", "TrainingData_401.txt"))
-    training_filename3 = path.abspath(path.join(dirname, "..", "data", "TrainingData_1601.txt"))
+    datahandler = Datahandler([training_filename_101, training_filename_401, training_filename_1601],
+                              ['101', '401', '1601'],
+                              testing_filename)
 
-    datahandler = Datahandler([training_filename1, training_filename2, training_filename3], ['101','401', '1601'], testing_filename)
+    iohandler_101 = IOHandler('101')
+    iohandler_401 = IOHandler('401')
+    iohandler_1601 = IOHandler('1601')
 
-    datahandler.create_submission([None,None,None])
+
+    datahandler.create_submission([iohandler_101.load_best_model(), iohandler_401.load_best_model(), iohandler_1601.load_best_model()])
+
+    datahandler.plot_data()
